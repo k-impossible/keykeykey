ebfc489a7189140500cef21ad40fce51749c0afc
k-impossible <kth3349@gmail.com>
2024-07-18 01:42:39 +0900

    robots meta 태그 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import App from "./App.tsx";
 import "./global.css";
 import { Toaster } from "sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 // import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
@@ -10,6 +11,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<QueryClientProvider client={queryClient}>
 		<App />
+
 		<Toaster position="bottom-center" richColors />
 		{/* <ReactQueryDevtools initialIsOpen={false} /> */}
 	</QueryClientProvider>
