54cb49f927c94deb43bf8c66a3aedb0aa75af375
k-impossible <kth3349@gmail.com>
2024-08-14 12:47:36 +0900

    [opt] vite-plugin-compression add

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
+import compression from "vite-plugin-compression";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		compression({ algorithm: "gzip" }),
+		compression({ algorithm: "brotliCompress", ext: ".br" }),
+	],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "src/"),
